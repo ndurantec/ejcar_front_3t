207eff2ff5bc0b4931d1635a096c60ef7a95b877
ADCR14 <a.rodrigues14@escola.pr.gov.br>
2025-10-30 10:16:25 -0300

    estruturando js

--- script/login.js
+++ script/login.js
@@ -26,13 +26,128 @@ function logar() {
        document.getElementById('erro-email').textContent = 'Preencha o email!';
     }
 
-       fetch('http://127.0.0.1:8080/responsaveis', { 
+    var headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    headers.append("Access-Control-Allow-Origin", "*");
+
+
+      fetch('http://127.0.0.1:8080/login/autenticar', { 
+
+         method: "GET",
+         mode: "cors",
+         cache: "no-cache",
+
+         body: JSON.stringify({
+
+            user: usuario,
+            email: email,
+            password: senha
+        }),
          
-    }).then(response => {
-           
-    }).then(data => {
+
+      headers: headers
+
+   }).then(response => {
+      if (!response.ok) {
+        throw new Error("Erro na resposta da API");
+      }
+      return response.json(); // <- converte o corpo da resposta em JSON
+    }).then(  data =>  {
+  
+      const usuario_id = data.id;
+      console.log("Id do registro buscado: ", usuario_id);
+  
+      localStorage.setItem('id_usuario', usuario_id);
+  
+    }).catch(error => console.error('Erro!:', error));
+
+
+
+
+   //  fetch("http://localhost:8080/login/1" , {
+ 
+   //    method: "DELETE",
+   //    mode: "cors",
+   //    cache: "no-cache",
+ 
+   //    body: JSON.stringify({
+   //      fluxo: "entrada",
+   //      step: "n",
+   //      chaveDeRoda: "n",
+   //      macaco: "n",
+   //      outro: "outro",
+   //      termo: "false",
+   //      equipamentos: "S",
+   //      idUsuario : 1,
+   //      servico : {
+   //          id : 1
+   //      }
+   //  }),
        
-    }).catch(error => {
-       
-    });
+   //    headers: headers
+
+   //  }).then(response => {
+   //    if (!response.ok) {
+   //      throw new Error("Erro ao excluir");
+   //    }
+   //    return response.json(); // <- converte o corpo da resposta em JSON
+   //  }).then(  data =>  {
+ 
+   //    const vistoria_id = data.id;
+   //    console.log("excluído com sucesso: ", vistoria_id);
+ 
+   //    localStorage.setItem('id_vistoria', vistoria_id);
+ 
+   //  }).catch(error => console.error('Erro ao excluir!:', error));
+  
 }
+
+function buscarVistoria() {
+
+   var headers = new Headers();
+   headers.append("Content-Type", "application/json");
+   headers.append("Access-Control-Allow-Origin", "*");
+
+   fetch("http://localhost:8080/vistoria/autenticar" , {
+
+     method: "GET",
+     mode: "cors",
+     cache: "no-cache",
+
+
+     body: JSON.stringify({
+
+      //     fluxo: "entrada",
+      //     step: "n",
+      //     chaveDeRoda: "n",
+      //     macaco: "n",
+      //     outro: "outro",
+      //     termo: "false",
+      //     equipamentos: "S",
+      //     idUsuario : 1,
+      //     servico : {
+      //         id : 1
+      //     }
+
+   }),
+
+     headers: headers
+
+
+   }).then(response => {
+     if (!response.ok) {
+       throw new Error("dados lidos!");
+     }
+     return response.json(); // <- converte o corpo da resposta em JSON
+   }).then(  data =>  {
+
+     const vistoria_id = data.id;
+     console.log("dados lidos com sucesso: ", vistoria_id);
+
+     localStorage.setItem('id_vistoria', vistoria_id);
+
+   }).catch(error => console.error('Erro ao ler os dados!:', error));
+
+}
+
+
--- script/vistoria.js
+++ script/vistoria.js
@@ -167,16 +167,124 @@ botaoConcluir.addEventListener('click', validarCheckbox);
 
 
 function concluir() {
-    fetch('http://127.0.0.1:8080/responsaveis', {
-       
-    }).then(response => {
-           
-    }).then(data => {
-       
-    }).catch(error => {
-       
-    });
+    function concluirVistoria() {
+
+        if (!validarFormulario()) return;
+    
+        const dados = coletarDados();
+        //console.log("Enviando criar conta:", dados);
+    
+        console.log(JSON.stringify(dados));
+    
+        var headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        headers.append("Access-Control-Allow-Origin", "*");
+    
+        fetch('http://localhost:8080/usuario/insert', {
+            
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            body: JSON.stringify(
+                dados
+            ),
+        
+            headers: headers
+    
+        })
+        .then(async response => {
+          let data = await response.json();
+    
+          console.log(data);
+          
+          if (!response.ok) {
+            // Caso sejam erros de validação no DTO
+            if (typeof data === "object") {
+              let mensagens = Object.values(data).join("<br>");
+    
+              console.log("Entrou dento do if data ==== object");
+              console.log("----------------------------------------------");
+              console.log(mensagens);
+              console.log("----------------------------------------------");
+    
+                let mensagensGlobais = []; // Para erros que não mapeiam para um campo específico
+    
+                for (const [campo, mensagem] of Object.entries(data)) {
+                    // Mapeia o nome do campo do backend ('cpf', 'email', etc.) para o ID do elemento no HTML
+                    const idElementoErro = "erro-" + campo; // Ex: 'cpf_error_message'
+    
+                    console.log("========================================================");
+                    console.log(idElementoErro);
+                    console.log("========================================================");
+                    // Tenta exibir o erro no elemento específico
+                    if (document.getElementById(idElementoErro)) {
+                        //CHAMANDO A SUA FUNÇÃO mostrarErro(idElemento, mensagem)
+                        mostrarErro(idElementoErro, mensagem);
+                                            
+                    }
+                }
+              
+            } else {
+              mostrarMensagem("⚠️ Erro desconhecido", "erro");
+            }
+            throw new Error("Erro de validação");
+          }
+    
+          return data;
+        })
+        .then(data => {
+          if (data.id) {
+            localStorage.setItem("id_professor", data.id);
+            // mostrarMensagem(data.message || "✅ Professor cadastrado com sucesso!", "sucesso");
+          }
+        })
+        .catch(error => console.error(error));
+    }
+    
 }
+
+
+
+function validarFormulario() {
+    //limparErros();
+    // Captura dos valores do formulário
+    let nome = document.getElementById("nome").value;
+    let cpf = document.getElementById("cpf").value;
+    let telefone = document.getElementById("telefone").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let confirme = document.getElementById("confirme").value;
+
+
+    let ok = true;
+
+    if (!nome) { mostrarErro('erro-nome', 'Verifique se possui nome para continuar.'); ok = false; }
+    if (!cpf) { mostrarErro('erro-cpf', 'Verifique se possui cpf para continuar.'); ok = false; }
+    if (!telefone) { mostrarErro('erro-telefone', 'Verifique se possui nome para continuar.'); ok = false; }
+    if (!email) { mostrarErro('erro-email', 'Verifique se possui email para continuar.'); ok = false; }
+    if (!senha) { mostrarErro('erro-senha', 'Verifique se possui senha para continuar.'); ok = false; }
+    if (!confirme) { mostrarErro('erro-confirme', 'Verifique se possui confirme senha para continuar.'); ok = false; }
+
+    return ok;
+}
+
+
+
+function coletarDados() {
+  
+    return {
+        nome: document.getElementById("nome").value.trim(),
+        cpf: document.getElementById("cpf").value.trim(),
+        telefone: document.getElementById("telefone").value.trim(),
+        email: document.getElementById("email").value.trim(),
+        user: document.getElementById("usuario").value.trim(),
+        password: document.getElementById("senha").value.trim(),
+        confirmarSenha: document.getElementById("confirme").value.trim(),
+        imagemBase64:  document.getElementById('signaturePad').toDataURL(),// converte assinatura para Base64
+
+    };
+}
+
 
 function salvar() {
 limparErros();
@@ -246,6 +354,9 @@ limparErros();
     });
 }
 
+
+
+
 function deletar() {
     fetch('http://localhost:8080/vistoria/{id}', {
        
@@ -257,6 +368,8 @@ function deletar() {
        
     });
 }
+
+
 
 function atualizar() {
     fetch('http://localhost:8080/vistoria/{id}', {
